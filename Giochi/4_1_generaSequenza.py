c82503a69d286703ca69e03ec63f755e72b23cc6
AlexBro98 <alexgasparini24@gmail.com>
2025-09-10 13:13:27 +0200

    Add files via upload

--- Giochi/4_1_generaSequenza.py
+++ Giochi/4_1_generaSequenza.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 while True:
-    try:
+    #try:
         os.system("clear")
         print("----------------------------------------------------------")
         print("Ora dovrai inserire i parametri e il seme della sequenza")
@@ -13,17 +13,20 @@ while True:
         print("")
         seed = int(input("inserisci il seme\n> "))
         
-        x = seed
+        values = [seed]
 
         os.system("clear")
-        print(f"Sequenza (a={a}, c={c}, m={m}, x_0={x}):\n{x}, ", end="")
-        while (a*x + c) % m != seed:
-            x = (a*x + c) % m
-            print(x, end=", ")
-        
-        x = (a*x + c) % m
+        print(f"Sequenza (a={a}, c={c}, m={m}, x_0={seed}): ")
 
-        print(x)
+        while (a*values[-1] + c) % m not in values:
+            values.append((a*values[-1] + c) % m)
+        
+        values.append(seed)
+
+        for i in range(len(values)-1):
+            print(values[i], end=", ")
+        print(values[-1])
+        
 
         choose = input("\n\nvuoi generare una nuova sequeza [Y/n]  \n> ")
 
@@ -31,5 +34,5 @@ while True:
             continue
         
         break
-    except:
+    #except:
         continue
